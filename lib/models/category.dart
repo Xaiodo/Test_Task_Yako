8783dc053dcbdbda2e1229a128883f3e80e81e57
Xaiodo <rostislavgovbak@gmail.com>
2023-05-12 17:59:54 +0300

    Created category model

---
+++ lib/models/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String name;
+  final Color primaryColor;
+  final Color secondaryColor;
+
+  const Category({
+    required this.name,
+    required this.primaryColor,
+    required this.secondaryColor,
+  });
+}
