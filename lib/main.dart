08d51ba8627db3cf0df48625a5b973781d246eeb
Xaiodo <rostislavgovbak@gmail.com>
2023-05-12 15:51:50 +0300

    Created app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: _appBar,
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
     );
   }
+
+  final AppBar _appBar = AppBar(
+    centerTitle: true,
+    title: const Text('Chat'),
+    backgroundColor: const Color(0xff0a2a3f),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.add_circle_outline_sharp),
+      ),
+    ],
+    leading: Container(
+      margin: const EdgeInsets.only(left: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: const CircleAvatar(
+        backgroundImage: NetworkImage(
+          'https://cdn.discordapp.com/attachments/1046399512526205038/1046414012713283594/p.jpg',
+        ),
+      ),
+    ),
+  );
 }
